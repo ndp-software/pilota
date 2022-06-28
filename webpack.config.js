ea5d09d11c436a880db5c1c0da7332fbf8581310
Andy Peterson <andy@ndpsoftware.com>
2022-06-27 19:21:30 -0700

    upgrade to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,46 @@
-var webpack        = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var path           = require('path');
-var mode           = require('yargs').argv.mode;
+var webpack        = require('webpack')
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+var path           = require('path')
+var mode           = require('yargs').argv.mode
 
-var libraryName = 'pilota';
+var libraryName = 'pilota'
 
-var plugins = [], outputFile;
+var plugins = [], outputFile
 
 if (mode === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize : true }));
-  outputFile = libraryName + '.min.js';
+  plugins.push(new UglifyJsPlugin({ minimize: true }))
+  outputFile = libraryName + '.min.js'
 } else {
-  outputFile = libraryName + '.js';
+  outputFile = libraryName + '.js'
 }
 
 var config = {
-  entry : __dirname + '/src/pilota.js',
-  output  : {
-    path           : __dirname + '/lib',
-    filename       : outputFile,
-    library        : libraryName,
-    libraryTarget  : 'umd',
-    umdNamedDefine : true
+  entry:   __dirname + '/src/pilota.js',
+  output:  {
+    path:           __dirname + '/lib',
+    filename:       outputFile,
+    library:        libraryName,
+    libraryTarget:  'umd',
+    umdNamedDefine: true,
   },
-  devtool : 'source-map',
-  module  : {
-    rules : [
+  devtool: 'source-map',
+  module:  {
+    rules: [
       {
-        test    : /\.js$/,
-        loader  : 'babel-loader',
-        exclude : /node_modules/
-      }
-    ]
+        test:    /\.js$/,
+        exclude: /node_modules/,
+        use:     {
+          loader:  'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+    ],
   },
-  plugins : plugins
-};
+  plugins: plugins,
+}
 
-module.exports = config;
+module.exports = config
